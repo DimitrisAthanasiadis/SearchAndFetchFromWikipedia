2f6ba5438f0cc604b14b667b5ca29fd25b97b5ce
Dimitris Athanasiadis <34347336+DimitrisAthanasiadis@users.noreply.github.com>
2018-12-02 21:53:49 +0200

    Add files via upload

---
+++ Search and fetch from Wikipedia/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Search_and_fetch_from_Wikipedia
+{
+    public partial class Search : Form
+    {
+        public Search()        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            var webclient = new WebClient();
+            var pageSourceCode = webclient.DownloadString("http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=" + textBox1.Text + "&redirects=true");
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(pageSourceCode);
+
+            var fnode = doc.GetElementsByTagName("extract")[0];
+
+            try
+            {
+                string ss = fnode.InnerText;
+                Regex regex = new Regex("\\<[^\\>]*\\>");
+                string.Format("Before:{0}", ss);
+                ss = regex.Replace(ss, string.Empty);
+                string result = String.Format(ss);
+                richTextBox1.Text += result;
+            }
+            catch(Exception)
+            {
+                richTextBox1.Text = "error";
+            }
+        }
+    }
+}
